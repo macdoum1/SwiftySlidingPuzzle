32702c4a3ff17eaaa3843655aa69b44dc835039b
mmacdougall <mmacdougall@etsy.com>
2015-12-17 10:20:09 -0500

    Initial Commit

---
+++ SwiftySlidingPuzzle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftySlidingPuzzle
+//
+//  Created by Mike MacDougall on 12/17/15.
+//  Copyright © 2015 Michael MacDougall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
